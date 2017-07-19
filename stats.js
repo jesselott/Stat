afe0a8db5d5568bdde6d438f2dc51c8ea78d8690
Jesse Lott <jesse.e.lott@outlook.com>
2017-07-18 23:30:19 -0400

    Add files via upload

---
+++ stats.js
@@ -0,0 +1,19 @@
+$('#btn1').click(function(){
+  $('#vanish1').toggleClass('show');
+  
+});
+
+$('#btn2').click(function(){
+  $('#vanish2').toggleClass('show');
+  
+});
+
+$('#btn3').click(function(){
+  $('#vanish3').toggleClass('show');
+  
+});
+
+$('#btn4').click(function(){
+  $('#vanish4').toggleClass('show');
+  
+});
