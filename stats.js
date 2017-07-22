eb7b562816a0631c352ec434c5a1ed18af02c05f
Jesse Lott <jesse.e.lott@outlook.com>
2017-07-21 21:16:05 -0400

    added scroll function to js.

--- stats.js
+++ stats.js
@@ -1,24 +1,36 @@
 $('#btn1').click(function(){
   $('#vanish1').toggleClass('show');
+  $('body').animate({
+        scrollTop: $("#vanish1").offset().top
+    }, 2000);
   
 });
 
 $('#btn2').click(function(){
   $('#vanish2').toggleClass('show');
-  
+  $('html, body').animate({
+        scrollTop: $("#vanish2").offset().top
+    }, 2000);
 });
 
 $('#btn3').click(function(){
   $('#vanish3').toggleClass('show');
-  
+  $('html, body').animate({
+        scrollTop: $("#vanish3").offset().top
+    }, 2000);
 });
 
 $('#btn4').click(function(){
   $('#vanish4').toggleClass('show');
-  
+  $('html, body').animate({
+        scrollTop: $("#vanish4").offset().top
+    }, 2000);
 });
 
 $('#btn5').click(function(){
   $('#conclusion').toggleClass('show');
-  
+  $('html, body').animate({
+        scrollTop: $("#conclusion").offset().top
+    }, 2000);
 });
+
